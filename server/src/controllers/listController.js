2d341a2c9dd1b9b4de0ece9f19ed168c5f4400cd
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-03-28 17:08:12 +0200

    delete get user by id function

--- server/src/controllers/listController.js
+++ server/src/controllers/listController.js
@@ -37,26 +37,7 @@ const listController = {
       res.status(500).json({ error: error.message });
     }
   },
-  getListById: async (req, res) => {
-    try {
-      const listId = req.params.id;
-      const userId = req.user.id;
 
-      // Retrieve the list from the database
-      const [list] = await pool.query(
-        "SELECT * FROM lists WHERE id = ? AND user_id = ?",
-        [listId, userId]
-      );
-
-      if (!list || list.length === 0) {
-        return res.status(404).json({ message: "List not found" });
-      }
-
-      res.status(200).json(list[0]);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
   getListsByUsername: async (req, res, next) => {
     try {
       const { error } = getListsByUsernameSchema.validate(req.params);
